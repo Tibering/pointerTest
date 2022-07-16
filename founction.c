6d223725d67fcd68823cbf543b6c096d82e98d41
Tibering <109405953+Tibering@users.noreply.github.com>
2022-07-16 21:02:16 +0800

    Update founction.c

--- founction.c
+++ founction.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 /// <summary>
-///	指针交换两个数的 
+///	鎸囬拡浜ゆ崲涓や釜鏁扮殑 
 /// </summary>
 /// <param name="pnum1"></param>
 /// <param name="pnum2"></param>
@@ -24,5 +24,77 @@ int passingMain(void) {
 	const int limit = 100;
 	int result = 5;
 	passingAddressOfConstants(&limit, &result);
+	return 0;
+}
+
+/// <summary>
+/// return pointer by founction.
+/// </summary>
+/// <param name="size"></param>
+/// <param name="value"></param>
+/// <returns></returns>
+
+int* allocateAray(int* arr, int size, int value) {
+	if (arr != NULL) {
+		for (size_t i = 0; i < size; i++) {
+			arr[i] = value;
+		}
+	}
+	return arr;
+}
+
+void allocateArray1(int** arr, int size, int value) {
+	*arr = (int*)malloc(size * sizeof(int));
+	if (*arr != NULL) {
+		for (size_t i = 0; i < size; i++) {
+			*(*arr + i) = value;
+		}
+	}
+}
+// int *vector = NULL;	allocateArray(&vector, 5, 4);
+
+// 鑷繁瀹炵幇free鍑芥暟.
+void saferFree(void** pp) {
+	if (pp != NULL && *pp != NULL) {
+		free(*pp);
+		*pp = NULL;
+	}
+}
+void arr1(int**, int, int);
+int main12(void)
+{
+	int* p;
+	arr1(&p, 5, 4);
+	for (int i = 0; i < 5; i++) {
+		printf("%d", p[i]);
+	}
+	return;
+}
+// **arr->*p; *arr == p
+void arr1(int** arr, int size, int val) {
+	*arr = (int*)malloc(size * sizeof(int));
+	if (*arr != NULL) {
+		for (int i = 0; i < size; i++){
+			*(*arr + i) = val;
+		}
+	}
+	printf("%p\t%p\t%p\n",arr, &(*arr), &(**arr));
+	printf("%d\t%d\t",*arr,**arr);
+}
+
+//鍑芥暟鎸囬拡
+int* f4();
+int (*f5)();
+int* (*f6)();
+
+int (*fptr1)(int);
+int square(int num) {
+	return num * num;
+}
+int main(void)
+{
+	int n = 5;
+	fptr1 = square;
+	printf("%d quared is %d\n", n, fptr1(n));
 	return;
 }
